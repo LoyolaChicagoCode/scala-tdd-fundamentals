05ce77f93a56c7c1ec613ae4aed78be403b42f6f
George K. Thiruvathukal <thiruvathukal@gmail.com>
2014-12-16 09:49:02 -0600

    Add Rational and RationalUnitTests
    Signed-off-by: George K. Thiruvathukal <thiruvathukal@gmail.com>

---
+++ src/main/scala/Rational.scala
@@ -0,0 +1,63 @@
+package scalatddpackt
+
+/*
+ * This version of Rational is based on Martin Odersky's Scala by Example.
+ * source: http://www.scala-lang.org/docu/files/ScalaByExample.pdf
+ *
+ * We include a number of additions/improvements based on Harrington and Thiruvathukal's C# Book,
+ * which includes an elaboration of other needed methods and full unit tests.
+ * source: http://introcs.cs.luc.edu
+ */
+object MathUtility {
+  def gcd(x: Int, y: Int): Int = {
+    if (x == 0) y
+    else if (x < 0) gcd(-x, y)
+    else if (y < 0) -gcd(x, -y)
+    else gcd(y % x, x)
+  }
+}
+
+class Rational(n: Int, d: Int) {
+
+  private val g = MathUtility.gcd(n, d)
+
+  val numerator: Int = n / g
+  val denominator: Int = d / g
+
+  def +(that: Rational) =
+    new Rational(numerator * that.denominator + that.numerator * denominator,
+      denominator * that.denominator)
+
+  def -(that: Rational) =
+    new Rational(numerator * that.denominator - that.numerator * denominator,
+      denominator * that.denominator)
+
+  def *(that: Rational) =
+    new Rational(numerator * that.numerator, denominator * that.denominator)
+
+  def /(that: Rational) =
+    new Rational(numerator * that.denominator, denominator * that.numerator)
+
+  def compare(that: Rational) = numerator * that.denominator - that.numerator * denominator
+
+  def ==(that: Rational) = compare(that) == 0
+
+  def reciprocal() =
+    new Rational(denominator, numerator)
+
+  def negate() =
+    new Rational(-numerator, denominator)
+
+  def >(that: Rational) =
+    compare(that) > 0
+
+  def <(that: Rational) =
+    compare(that) < 0
+
+  def >=(that: Rational) =
+    this > that || this == that
+
+  def <=(that: Rational) =
+    this < that || this == that
+
+}
---
+++ src/test/scala/RationalTests.scala
@@ -0,0 +1,48 @@
+package scalatddpackt;
+
+import org.junit._
+import org.junit.Test
+import org.junit.Assert._
+
+
+class RationalTests {
+
+  @Test
+  def testMathUtilities(): Unit = {
+    assertEquals(3, MathUtility.gcd(3, 6))
+    assertEquals(3, MathUtility.gcd(-3, 6))
+    assertEquals(-3, MathUtility.gcd(-3, -6))
+    assertEquals(5, MathUtility.gcd(0, 5));
+    assertEquals(5, MathUtility.gcd(5, 0));
+    assertEquals(1, MathUtility.gcd(1, 5));
+    assertEquals(1, MathUtility.gcd(5, 1));
+  }
+
+  @Test
+  def testInitialization(): Unit = {
+    val r1 = new Rational(2, 4)
+    assertEquals(1, r1.numerator)
+    assertEquals(2, r1.denominator)
+
+    val r2 = new Rational(-3, 6)
+    assertEquals(-1, r2.numerator)
+    assertEquals(2, r2.denominator)
+
+    val r3 = new Rational(-3, -6)
+    assertEquals(1, r3.numerator)
+    assertEquals(2, r3.denominator)
+  }
+
+  @Test
+  def TestArithmetic(): Unit = {
+    val r1 = new Rational(47, 64)
+    val r2 = new Rational(-11, 64)
+
+    assert(r1 + r2 == new Rational(36, 64))
+    assert(r1 - r2 == new Rational(58, 64))
+    assert(r1 * r2 == new Rational(47 * -11, 64 * 64))
+    assert(r1 / r2 == new Rational(47, -11))
+    assert(r2.reciprocal() == new Rational(64, -11))
+    assert(r2.negate() == new Rational(11, 64))
+  }
+}
