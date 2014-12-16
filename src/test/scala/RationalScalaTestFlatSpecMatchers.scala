41b0e4130bf7ea5397c98665e5125c300241e3fe
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-12-16 12:56:41 -0600

    Moving from JUnit to FlatSpec-style testing
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- src/test/scala/RationalScalaTestFlatSpecMatchers.scala
+++ src/test/scala/RationalScalaTestFlatSpecMatchers.scala
@@ -32,4 +32,85 @@ class RationalScalaTestFlatSpecMatchers extends FlatSpec with Matchers {
     r1.numerator should be (1)
     r1.denominator should be (2)
   }
+
+  "Essential Arithmetic" should "perform addition" in {
+    val r1 = new Rational(47, 64)
+    val r2 = new Rational(-11, 64)
+    r1 + r2 should === (new Rational(36, 64))
+    r1 + r2 should be (new Rational(36, 64))
+  }
+
+  it should "perform subtraction" in {
+    val r1 = new Rational(47, 64)
+    val r2 = new Rational(-11, 64)
+    r1 - r2 should be (new Rational(58, 64))
+  }
+
+  it should "perform multiplication" in {
+    val r1 = new Rational(47, 64)
+    val r2 = new Rational(-11, 64)
+    r1 * r2 should be (new Rational(47 * -11, 64 * 64))
+  }
+
+  it should "perform division" in {
+    val r1 = new Rational(47, 64)
+    val r2 = new Rational(-11, 64)
+    r1 / r2 should be (new Rational(47, -11))
+  }
+
+  it should "perform the reciprocal" in {
+    val r1 = new Rational(47, 64)
+    r1.reciprocal() should be (new Rational(64, 47))
+  }
+
+  it should "perform negation" in {
+    val r1 = new Rational(47, 64)
+    val r2 = new Rational(-11, 64)
+    r1.negate() should be (new Rational(-47, 64))
+  }
+
+  "Essential Comparisons" should "perform ==" in {
+    val r1 = new Rational(2, 4)
+    val r2 = new Rational(1, 2)
+    assert(r1 == r2)
+    info("numeric equality (==) works")
+    assert(r1.equals(r2))
+    info("object equality works, e.g. equals()")
+    assert(r1.hashCode() == r2.hashCode())
+    info("hashcode() works")
+
+  }
+  "Essential Comparisons" should "perform <" in {
+    val r1 = new Rational(-3, 6)
+    val r2 = new Rational(2, 4)
+    assert(r1 < r2)
+    info("operator < works")
+  }
+
+  it should "perform >" in {
+    val r1 = new Rational(-3, 6)
+    val r2 = new Rational(2, 4)
+    assert(r2 > r1)
+    info("operator > works")
+  }
+
+  it should "perform <= for something < and for something =" in {
+    val r1 = new Rational(-3, 6)
+    val r2 = new Rational(2, 4)
+    val r3 = new Rational(1, 2)
+    assert(r1 <= r2)
+    info("operator <= works for < case")
+    assert(r2 <= r3)
+    info("operator <= works for == case")
+  }
+
+  it should "perform >= for something > and for something =" in {
+    val r1 = new Rational(-3, 6)
+    val r2 = new Rational(2, 4)
+    val r3 = new Rational(1, 2)
+    assert(r2 >= r1)
+    info("operator >= works for > case")
+    assert(r2 >= r3)
+    info("operator >= works for == case")
+  }
 }
